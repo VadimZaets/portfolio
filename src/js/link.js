9cdad8176c9154c07e92d57f57729fe4b2a7fd34
Vadim Zaets <79648897+VadimZaets@users.noreply.github.com>
2022-04-14 20:45:25 +0300

    fix

--- src/js/link.js
+++ src/js/link.js
@@ -1,9 +1,14 @@
 const links = document.querySelectorAll('a[href*="#"]');
-
+const linkMenuRef = document.querySelector('[data-menu]');
+const menuBtnRef = document.querySelector('[data-menu-button]');
 for (let link of links) {
   link.addEventListener('click', function (e) {
     e.preventDefault();
+    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+    linkMenuRef.classList.toggle('is-open');
     const blockID = link.getAttribute('href').substr(1);
 
     document.getElementById(blockID).scrollIntoView({
